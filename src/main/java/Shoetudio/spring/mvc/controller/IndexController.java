3908160efc541e9db68690b9ed5705cd44a6547d
wongahee <rkgml284@gmail.com>
2021-06-29 16:10:09 +0900

    Project Backend Controller - index, Custom, ContactUs dao/service/vo/mapper - Custom, ContactUs

--- src/main/java/Shoetudio/spring/mvc/controller/IndexController.java
+++ src/main/java/Shoetudio/spring/mvc/controller/IndexController.java
@@ -1,12 +1,39 @@
 package Shoetudio.spring.mvc.controller;
 
+import Shoetudio.spring.mvc.service.UserService;
+import Shoetudio.spring.mvc.vo.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class IndexController {
+
+    @Autowired private UserService usrv;
+
     @GetMapping("/")
-    public String index() {
-        return "index";
+    public String index(){
+        return "index.tiles";
     }
+
+    @PostMapping("/login")
+    public String login(User u, HttpSession sess) {
+        String returnPage = "redirect:/join/loginfail";
+
+        if(usrv.checkLogin(u,sess))    // 로그인 성공 시
+            returnPage = "redirect:/";
+
+        return returnPage;
+    }
+
+    @GetMapping("/join/logout")
+    public String logout(HttpSession sess){
+        sess.invalidate();  // 세션객체 제거
+
+        return "redirect:/";
+    }
+
 }
---
+++ src/main/java/Shoetudio/spring/mvc/service/ContactUsService.java
@@ -0,0 +1,8 @@
+package Shoetudio.spring.mvc.service;
+
+import Shoetudio.spring.mvc.vo.ContactUs;
+
+public interface ContactUsService {
+
+    boolean newContact(ContactUs cu);
+}
